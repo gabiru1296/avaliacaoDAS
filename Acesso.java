3beac322e1ee5c675b0ea191e99746b82eee5890
Gabriel Araújo <gabiru1296@gmail.com>
2017-05-03 16:59:05 -0300

    Questão 2 - modificação 1

--- Acesso.java
+++ Acesso.java
@@ -4,60 +4,67 @@ public class Acesso {
 	public static final float VALOR_FRACAO = (float) 2.0;
 	public static final float VALOR_HORA = (float) 7.0;
 	public static final float VALOR_DIARIA = (float) 30.0;
-	
+
 	public String placa;
 	public int dia, mes, ano;
-	public int horaEntrada, 
+	public int horaEntrada,
 	           minutosEntrada;
-	public int horaSaida, 
+	public int horaSaida,
 			   minutosSaida;
-	
+
 
 
 	public Acesso() {}
 
-	
-	public Acesso(int dia, int mes, int ano, int horaEntrada, int minutosEntrada) { 
+
+	public Acesso(int dia, int mes, int ano, int horaEntrada, int minutosEntrada) {
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
 		this.horaEntrada = horaEntrada;
 		this.minutosEntrada = minutosEntrada;
 	}
-	
-	
-	public float calcularValor() { 
-		int quantidadeHoras = horaSaida - horaEntrada; 
-		int quantidadeMinutos; 
-		
-		if (horaSaida == horaEntrada)
-			quantidadeMinutos = minutosSaida - minutosEntrada;
-		else if (horaSaida > horaEntrada && minutosEntrada == minutosSaida){
-			quantidadeMinutos = 0;
+
+
+	public float calcularValor() {
+		final int quantidadeHoras;
+		final int quantidadeMinutos;
+
+		// Sequencia lógica de if e else, ou seja, cada variavel temporária
+		// será inicializada apenas uma vez. Não mudando de valor com o tempo.
+		if (horaSaida == horaEntrada) {
 			quantidadeHoras = horaSaida - horaEntrada;
-		}
-		else if (horaSaida > horaEntrada && minutosEntrada > minutosSaida) 
 			quantidadeMinutos = minutosSaida - minutosEntrada;
+		}
+		else if (horaSaida > horaEntrada && minutosEntrada == minutosSaida) {
+			quantidadeHoras = horaSaida - horaEntrada;
+			quantidadeMinutos = 0;
+		}
+		else if (horaSaida > horaEntrada && minutosEntrada > minutosSaida) {
+			quantidadeHoras = horaSaida - horaEntrada;
+			quantidadeMinutos = minutosSaida - minutosEntrada;
+		}
 		else if (horaSaida > horaEntrada && minutosSaida < minutosEntrada){
-			quantidadeMinutos = minutosSaida + (60 - minutosEntrada);
 			quantidadeHoras = horaSaida - horaEntrada - 1;
+			quantidadeMinutos = minutosSaida + (60 - minutosEntrada);
 		}
 		else {
 			quantidadeHoras = 0;
 			quantidadeMinutos = 0;
 		}
-		
-		float valorTotal = 0; 
+
+		// somatório não precisa aplicar Dividir variável
+		float valorTotal = 0;
 		valorTotal += quantidadeHoras * VALOR_HORA;
-		valorTotal += Math.ceil(quantidadeMinutos / 15.0) * VALOR_FRACAO;		
-		
+		valorTotal += Math.ceil(quantidadeMinutos / 15.0) * VALOR_FRACAO;
+
 		if (quantidadeHoras >=9)
 			return VALOR_DIARIA;
-		else 
+		else
 			return valorTotal;
 	}
-	
-	
+
+
 	public void setHoraSaida(int horaSaida) {
 		this.horaSaida = horaSaida;
 	}
@@ -66,7 +73,7 @@ public class Acesso {
 	public void setMinutosSaida(int minutosSaida) {
 		this.minutosSaida = minutosSaida;
 	}
-	
-	
-	
+
+
+
 }
